f01bf8e1e37ad2a67b66ad9783ad4255d2e4c23f
baileyshelden <bailey7748@gmail.com>
2018-06-04 21:55:42 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,16 @@
+def about_me
+  puts "My name is Bailey"
+  puts "I am 13 years old."
+end
+
+about_me
+about_me 
+
+def bailey 
+  puts "my name is bailey"
+  puts "golden retreivers are the best dog"
+end
+  
+bailey 
+bailey
+about_me
